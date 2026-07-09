06154a16a173c3a4136c28a65b7e0513d910837b
Muhammad Haris <mh723595@gmail.com>
2026-07-09 10:46:57 +0700

    update: MainActivity

--- app/src/main/java/com/example/calbudget/MainActivity.kt
+++ app/src/main/java/com/example/calbudget/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.calbudget.core.navigation.FinanceNavGraph
 import com.example.calbudget.core.theme.FinanceAppTheme
+import com.example.calbudget.data.repository.SettingsRepository
+import com.example.calbudget.domain.model.AppSettings
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var settingsRepository: SettingsRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // installSplashScreen() HARUS dipanggil SEBELUM super.onCreate()
         installSplashScreen()
@@ -21,7 +30,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            FinanceAppTheme {
+            val settings by settingsRepository.settingsFlow
+                .collectAsState(initial= AppSettings())
+            FinanceAppTheme(darkTheme = settings.isDarkMode) {
                 FinanceNavGraph()
             }
         }
